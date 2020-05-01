df519270a7fc85dd5c4e8ec555305dce4adf3a25
lyabs243 <loicyabili@outlook.com>
2020-05-01 13:12:42 +0200

    Init admin template

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,8 +5,19 @@
  * Date: 01/05/2020
  * Time: 10:13
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin
-{
+class Admin extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+		//Loading url helper
+		$this->load->helper('url');
+	}
+
+	public function index()
+	{
+		$this->load->view('admin_panel');
+	}
 }
