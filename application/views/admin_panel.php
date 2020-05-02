915586b6cf078535bc88bc602f4ace5514f1308c
lyabs243 <loicyabili@outlook.com>
2020-05-02 09:30:04 +0200

    Create separate view for highly comprehension

--- application/views/admin_panel.php
+++ application/views/admin_panel.php
@@ -1,110 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Millionaire Quiz | Dashboard</title>
-	<!-- Tell the browser to be responsive to screen width -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/fontawesome-free/css/all.min.css">
-	<!-- Ionicons -->
-	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-	<!-- Tempusdominus Bbootstrap 4 -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-	<!-- iCheck -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-	<!-- JQVMap -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/jqvmap/jqvmap.min.css">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/adminlte.min.css">
-	<!-- overlayScrollbars -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-	<!-- Daterange picker -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/daterangepicker/daterangepicker.css">
-	<!-- summernote -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>plugins/summernote/summernote-bs4.css">
-	<!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
+<?php
+    $data['title'] = 'Millionaire Quiz | Dashboard';
+    $this->view('admin_head', $data);
+?>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
-	<!-- Navbar -->
-	<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-		<!-- Left navbar links -->
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-			</li>
-			<li class="nav-item d-none d-sm-inline-block">
-				<a href="index3.html" class="nav-link">Home</a>
-			</li>
-			<li class="nav-item d-none d-sm-inline-block">
-				<a href="#" class="nav-link">Questions</a>
-			</li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Users</a>
-            </li>
-		</ul>
-	</nav>
-	<!-- /.navbar -->
-
-	<!-- Main Sidebar Container -->
-	<aside class="main-sidebar sidebar-dark-primary elevation-4">
-		<!-- Brand Logo -->
-		<a href="index3.html" class="brand-link">
-			<img src="<?php echo base_url(); ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-			     style="opacity: .8">
-			<span class="brand-text font-weight-light">Millionaire Quiz</span>
-		</a>
-
-		<!-- Sidebar -->
-		<div class="sidebar">
-			<!-- Sidebar user panel (optional) -->
-			<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-				<div class="info">
-					<a href="#" class="d-block"><?php echo $user->first_name . ' ' . $user->last_name; ?></a>
-				</div>
-			</div>
-
-			<!-- Sidebar Menu -->
-			<nav class="mt-2">
-				<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-					<!-- Add icons to the links using the .nav-icon class
-						 with font-awesome or any other icon font library -->
-
-					<li class="nav-item">
-						<a href="pages/gallery.html" class="nav-link">
-							<i class="nav-icon far fa-question-circle"></i>
-							<p>
-								Manage Questions
-							</p>
-						</a>
-					</li>
-                    <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
-                            <i class="nav-icon far fa-user"></i>
-                            <p>
-                                Manage Users
-                            </p>
-                        </a>
-                    </li>
-					<li class="nav-header">USER</li>
-                    <li class="nav-item">
-                        <a href="<?php echo base_url(); ?>index.php/user/logout/" class="nav-link">
-                            <i class="nav-icon far fa fa-sign-out-alt"></i>
-                            <p>
-                                Logout
-                            </p>
-                        </a>
-                    </li>
-				</ul>
-			</nav>
-			<!-- /.sidebar-menu -->
-		</div>
-		<!-- /.sidebar -->
-	</aside>
+	<?php $this->view('admin_navbar'); ?>
 
 	<!-- Content Wrapper. Contains page content -->
 	<div class="content-wrapper">
@@ -240,20 +143,7 @@
 		</section>
 		<!-- /.content -->
 	</div>
-	<!-- /.content-wrapper -->
-	<footer class="main-footer">
-		<strong>Copyright &copy; 2020 Millionaire Quiz</strong>
-		All rights reserved.
-		<div class="float-right d-none d-sm-inline-block">
-			<b>Version</b> 1.0.0
-		</div>
-	</footer>
-
-	<!-- Control Sidebar -->
-	<aside class="control-sidebar control-sidebar-dark">
-		<!-- Control sidebar content goes here -->
-	</aside>
-	<!-- /.control-sidebar -->
+	<?php $this->view('admin_footer'); ?>
 </div>
 <!-- ./wrapper -->
 
