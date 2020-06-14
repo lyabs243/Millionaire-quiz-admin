9f1526837410f3a70ef07e81c09df5d34b342f49
lyabs243 <loicyabili@outlook.com>
2020-06-14 18:35:12 +0200

    Manager players score to show top weekly and monthly leadboard

---
+++ application/models/Player_score_model.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyabs
+ * Date: 14/06/2020
+ * Time: 10:17
+ */
+
+class Player_score_model extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function add_score($idAccount, $score)
+	{
+		$data['id_account'] = $idAccount;
+		$data['score'] = $score;
+		$this->db->insert('player_score', $data);
+		$id = $this->db->insert_id();
+		return $id;
+	}
+
+	//get leadboard: per week or mothly
+	function get_player_score($start, $length, $perWeek=1)
+	{
+		$sql = 'SELECT ps.id_account, p.full_name, p.url_profil_pic, SUM(ps.score) as total
+				FROM player_score ps
+				JOIN player p
+				ON ps.id_account = p.id_account
+				';
+		if ($perWeek) {
+			$sql .= ' WHERE yearweek(DATE(ps.register_date), 1) = yearweek(curdate(), 1) ';
+		}
+		else {
+			$sql .= 'WHERE MONTH(ps.register_date) = MONTH(CURRENT_DATE())
+				AND YEAR(ps.register_date) = YEAR(CURRENT_DATE()) ';
+		}
+		$sql .= ' GROUP BY ps.id_account
+				ORDER BY total DESC
+				LIMIT ?, ?';
+
+		$args = array($start, $length);
+
+		$query = $this->db->query($sql, $args);
+		return $query->result();
+	}
+
+	function get_answers($idQuestion)
+	{
+		$sql = 'SELECT * 
+		FROM answer a 
+		WHERE id_question = ? ';
+		$args = array($idQuestion);
+		$query = $this->db->query($sql, $args);
+		return $query->result();
+	}
+}
